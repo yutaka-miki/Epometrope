c08b31a96a3e6cc0a53c14db41e01fe7c3a81a1a
Yutaka-Miki <48874232+yutaka-miki@users.noreply.github.com>
2020-10-03 13:51:11 +0900

    Add files via upload

---
+++ Arduino/base-part/base-part.ino
@@ -0,0 +1,18 @@
+void setup() {
+  
+  Serial.begin(9600);  
+  
+}
+
+void loop(){
+  
+  int val;
+  val = analogRead(7);
+  
+  //If necessary, you need to adjust the value of "map".
+  val = map(val, 0, 1023, 0, 210);
+  
+  Serial.println(val); 
+  analogWrite(3, val);
+
+}
